40bcea1f5f8952d1eb138bf1c51e4b3793029a1b
Tony Bowden <tony@tmtm.com>
2021-09-21 08:37:49 +0300

    Skip Deputy PM
    This role isn't listed on the ministers page.

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,12 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed on the site
+SKIP = [
+  ['---', 'Annika Saarikko', 'Suomen pääministerin sijainen']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
+
 puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
